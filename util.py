b04cc2cf76cd92c7a2e70f7712b4cfa949ff32a3
Shagun Sodhani <sshagunsodhani@gmail.com>
2018-03-08 14:46:41 -0500

    Added basic logic to fine-tune a pretrained model and to display results

---
+++ util.py
@@ -0,0 +1,28 @@
+# constants
+RESNET50 = "resnet50"
+RESNET101 = "resnet101"
+VGG16 = "vgg16"
+EPOCH = "epoch"
+MODEL = "model"
+STATE_DICT = "state_dict"
+VAL_ACCURACY = "val_accuracy"
+BEST_VAL_ACCURACY = "best_val_accuracy"
+VAL_LOSS = "val_loss"
+BEST_VAL_LOSS = "best_val_loss"
+TRAIN = "train"
+TEST = "test"
+VAL = "val"
+
+# config
+train_dir = "data/train/"
+val_dir = "data/val"
+test_dir = "data/test"
+batch_size = 32
+image_size = 224
+num_workers = 2
+model_name = RESNET101
+use_cuda = True
+num_epochs = 40
+learning_rate = 1e-3
+early_stopping_criteria = 5
+
